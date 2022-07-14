7bcf83a48be72c46bf56d2f72577c38dc97f5dc8
RubenVercaemst <ruben.vercaemst@gmail.com>
2022-07-14 19:37:14 +0200

    Backup before rewrite

--- src/app/views/builder/builder.component.ts
+++ src/app/views/builder/builder.component.ts
@@ -10,10 +10,25 @@ export class BuilderComponent implements OnInit {
   @Input() selectedGuardians: any;
 
   selected: any;
+  selectedTeam: any;
+
+  selectedTalents: [any?, any?][] = [[],[],[]];
+
+  //selectedGuardians: any = [[],[],[]];
+
+  //{ name: 'John', age: 30 }
+
+  mapGuardians = new Map([
+
+  ])
+
+  myarray: any[] = [];
+  priceListMap : Map<string, any[]> = new Map<string, any[]>();
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
   removeSelectedGuardian(guardian: any){
@@ -22,6 +37,30 @@ export class BuilderComponent implements OnInit {
   }
 
   detailsSelectedGuardian(guardian: any){
+  }
+
+  testclick(talent: any, guardian: any){
+    console.log(guardian)
+    console.log(talent)
+    //this.selectedTalents.push(talent)
+    //this.selectedTalents.push([guardian.id, talent.title])
+    //this.mapGuardians.set(guardian.id, talent)
+    //console.log(this.mapGuardians)
+
+    //this.myarray.push(talent)
+    if(this.priceListMap.has(guardian.id)){
+      //key already exists
+      for (let entry of this.priceListMap.entries()) {
+        //console.log(entry[0], entry[1]);
+        if(entry[0]===guardian.id){
+          entry[1].push(talent)
+        }
+      }
+    } else {
+      this.priceListMap.set(guardian.id, talent)
+    }
+
+    console.log(this.priceListMap)
   }
 }
 
