c1e6c1cc2343114bd2e21b27a411d4b1be6c28d6
Ruben Vercaemst <ruben.vercaemst@gmail.com>
2023-07-04 16:51:43 -0400

    Reworked dropdown

--- src/app/builder/ui/talent-dropdown/talent-dropdown.component.ts
+++ src/app/builder/ui/talent-dropdown/talent-dropdown.component.ts
@@ -7,21 +7,15 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 })
 export class TalentDropdownComponent implements OnInit {
   options = ['Option 1', 'Option 2', 'Option 3'];
-  showDropdown = false;
   selectedOption: any;
   currentlySelectedOption: any;
   showTooltip = false;
-
-  toggleDropdown() {
-    this.showDropdown = !this.showDropdown;
-  }
 
   selectOption(option: any) {
     if(this.currentlySelectedOption != undefined){
       this.currentlySelectedOption.selected = false;
     }
     this.selectedOption = option;
-    this.toggleDropdown();
     this.optionSelected.emit(option);
     this.currentlySelectedOption = option;
   }
