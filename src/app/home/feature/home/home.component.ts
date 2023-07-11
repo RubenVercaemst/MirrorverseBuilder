ee626c57a303b69d08f35fd4f205e328c3687f74
Ruben Vercaemst <ruben.vercaemst@gmail.com>
2023-07-11 15:29:51 -0400

    Added a hero section

--- src/app/home/feature/home/home.component.ts
+++ src/app/home/feature/home/home.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   constructor() {}
+
+  scrollToBuilder($element: any): void {
+    $element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
 }
